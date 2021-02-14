f45cb7439bc8752fadb06173ab920640c949b613
boole.george.john@gmail.com <boole.george.john@gmail.com>
2021-02-14 01:25:40 +0000

    added scroll to about section on clicking scroll icon

--- js/scroll.js
+++ js/scroll.js
@@ -16,6 +16,15 @@ $(".goto-top").on("click", function () {
   );
 });
 
+$(".goto-about").on("click", function () {
+  $("html, body").animate(
+    {
+      scrollTop: $(this.hash).offset().top + 190,
+    },
+    1000
+  );
+});
+
 // $(document).ready(function(){
 //     $('.popup__img').css('visibility', 'visible');
 // })
